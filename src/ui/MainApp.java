aa01427c1af2f44a60636256d0c252ddc7981a31
Anak Agung Gede Putu Wiradarma <gungwira@Anaks-MacBook-Air.local>
2025-11-30 13:06:57 +0800

    Finish login form

---
+++ src/ui/MainApp.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author gungwira
+ */
+
+package ui;
+
+public class MainApp {
+    public static void main(String[] args) {
+        // buka halaman login
+        new LoginForm().setVisible(true);
+    }
+}
