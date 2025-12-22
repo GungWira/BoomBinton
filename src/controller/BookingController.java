8a736d2913ac8444d202610ce465c7734d9a792b
Anak Agung Gede Putu Wiradarma <gungwira@Anaks-MacBook-Air.local>
2025-12-22 10:37:53 +0800

    remove unused method on booking controller and adding documentation on code

--- src/controller/BookingController.java
+++ src/controller/BookingController.java
@@ -13,12 +13,14 @@ import model.Booking;
  * @author gungwira
  */
 public class BookingController {
-    private BookingDAO bookingDAO;
+    final private BookingDAO bookingDAO;
     
+    // method constructor untuk booking controller
     public BookingController(){
         this.bookingDAO = new BookingDAO();
     }
     
+    // method untuk menambahkan booking baru
     public BookingRes addBooking(Booking booking){
         boolean res = bookingDAO.createBooking(booking);
 
@@ -29,24 +31,27 @@ public class BookingController {
         return new BookingRes(false, "Gagal menambahkan booking", null);
     }
     
+    // method untuk mengambil data booking hari ini
     public List<Booking> getTodayBookings() {
         return bookingDAO.getTodayBookings();
     }
 
+    // method untuk mengambil data booking bulan ini
     public List<Booking> getMonthlyBookings() {
         return bookingDAO.getMonthlyBookings();
     }
 
+    // method untuk mengambil data booking tahun ini
     public List<Booking> getYearlyBookings() {
         return bookingDAO.getYearlyBookings();
     }
 
-    
+    // static class untuk return type booking
     public static class BookingRes {
 
-        private boolean success;
-        private String message;
-        private Booking booking;
+        final private boolean success;
+        final private String message;
+        final private Booking booking;
 
         public BookingRes(boolean success, String message, Booking booking) {
             this.success = success;
